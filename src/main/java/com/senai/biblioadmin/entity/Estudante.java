a9c2317729f48248cd0aca421c38c2ed780c525a
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-06-17 09:02:29 -0300

    SALVO

--- src/main/java/com/senai/biblioadmin/entity/Estudante.java
+++ src/main/java/com/senai/biblioadmin/entity/Estudante.java
@@ -1,13 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.senai.biblioadmin.entity;
 
-/**
- *
- * @author joao_cappeletti
- */
+import jakarta.persistence.Entity;
+
+@Entity
 public class Estudante {
+
+    public Long getIdEstudante() {
+        return IdEstudante;
+    }
+
+    public void setIdEstudante(Long IdEstudante) {
+        this.IdEstudante = IdEstudante;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public Long getEmail() {
+        return email;
+    }
+
+    public void setEmail(Long email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    private Long IdEstudante;
+    
+    private String nome;
+    
+    private Long matricula;
+    
+    private Long email;
+    
+    private String telefone;
+
+    private String senha;
     
 }
