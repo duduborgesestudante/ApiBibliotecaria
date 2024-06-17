205ccf1a93d54756088d94302ae27766bb83feba
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2024-06-17 09:18:27 -0300

    atualizando

--- src/main/java/com/senai/biblioadmin/entity/Emprestimo.java
+++ src/main/java/com/senai/biblioadmin/entity/Emprestimo.java
@@ -1,11 +1,87 @@
 
 package com.senai.biblioadmin.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.util.Date;
+
 
 @Entity
 @Table(name = "emprestimos")
 public class Emprestimo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long IdEmprestimo;
+     
+    @OneToOne
+    private Livro livro;
+    
+    @Column(nullable = false)
+    private Long matricula;
+    
+    @Column(nullable = false)
+    private Date dataEmprestimo;
+    
+    @Column(nullable = true)
+    private Date dataEntrega;
+    
+    @Column(nullable = true)
+    private String devolucao;
+
+    public Long getIdEmprestimo() {
+        return IdEmprestimo;
+    }
+
+    public void setIdEmprestimo(Long IdEmprestimo) {
+        this.IdEmprestimo = IdEmprestimo;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(Date dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getDevolucao() {
+        return devolucao;
+    }
+
+    public void setDevolucao(String devolucao) {
+        this.devolucao = devolucao;
+    }
+    
+    
     
 }
