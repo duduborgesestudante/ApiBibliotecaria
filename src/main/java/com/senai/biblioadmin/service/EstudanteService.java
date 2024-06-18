2243c3f19fa5c156ed5d221e733d713bdd3c41e2
LUCA OLANDIM MELLO <luca_mello@estudante.sesisenai.org.br>
2024-06-18 11:03:43 -0300

    criando service

---
+++ src/main/java/com/senai/biblioadmin/service/EstudanteService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senai.biblioadmin.service;
+
+/**
+ *
+ * @author luca_mello
+ */
+public class EstudanteService {
+    
+}
