f1b89a5bcc81593a2fd8feb1e8f5a1b4ffa05d53
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-06-18 12:25:15 -0300

    estudante service atualizado

--- src/main/java/com/senai/biblioadmin/service/EstudanteService.java
+++ src/main/java/com/senai/biblioadmin/service/EstudanteService.java
@@ -1,13 +1,81 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.senai.biblioadmin.service;
 
-/**
- *
- * @author luca_mello
- */
+import com.senai.biblioadmin.entity.Estudante;
+import com.senai.biblioadmin.repository.EstudanteRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EstudanteService {
+    private EstudanteRepository estudanteRepository;
     
+     public Long incluirEstudante(Estudante estudante){
+        if(estudante.getNome() == null || estudante.getNome() == ""){   
+            return null;
+        }
+        if(estudante.getEmail() == null || estudante.getEmail() == ""){
+            return null;
+        }
+        if(estudante.getMatricula() == null){
+            return null;
+        }
+        if(estudante.getSenha() == null || estudante.getSenha() == ""){
+            return null;
+        }
+        if(estudanteRepository.findByMatricula(estudante.getMatricula()) == null){
+            return estudanteRepository.save(estudante).getIdEstudante();
+        }
+        return null;
+    }
+    
+    public boolean excluirEstudante(Long IdEstudante){
+        if(estudanteRepository.findById(IdEstudante).isPresent()){
+            estudanteRepository.deleteById(IdEstudante);
+            return true;
+        }
+        return false;
+    }
+    
+    public Estudante consultaEstudantePorId(Long IdEstudante){
+        return estudanteRepository.findById(IdEstudante).get();
+    }
+    
+    public Estudante consultaEstudantePorMatricula(Long matricula){
+        return estudanteRepository.findByMatricula(matricula);
+    }
+    
+    public List<Estudante> listarEstudantes(){
+        
+        return estudanteRepository.findAll();
+    }
+    
+    public boolean alterarEstudante(Estudante estudante){
+
+        if(estudante.getNome() == null || estudante.getNome() == ""){
+            return false;
+        }
+        if(estudante.getEmail() == null || estudante.getEmail() == ""){
+            return false;
+        }
+        if(estudante.getMatricula() == null){
+            return false;
+        }
+        
+        Estudante estBD = estudanteRepository.getReferenceById(estudante.getIdEstudante());
+        if(estBD != null){
+            if(estBD.getMatricula() != estudante.getMatricula() &&
+                estudanteRepository.findByMatricula(estudante.getMatricula()) != null){
+                return false;
+            }
+            estBD.setTelefone(estudante.getTelefone());
+            estBD.setMatricula(estudante.getMatricula());
+            estBD.setEmail(estudante.getEmail());
+            estBD.setSenha(estudante.getSenha());
+            estBD.setNome(estudante.getNome());
+            estudanteRepository.save(estBD);
+            return true;           
+            }
+            return false;
+        }
 }
