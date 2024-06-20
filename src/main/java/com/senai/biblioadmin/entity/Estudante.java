d9d1c9b0aa24e7aa3ac64c8b061b617f111ed446
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2024-06-20 10:51:32 -0300

    atualizando estudante

--- src/main/java/com/senai/biblioadmin/entity/Estudante.java
+++ src/main/java/com/senai/biblioadmin/entity/Estudante.java
@@ -3,8 +3,9 @@ package com.senai.biblioadmin.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,7 +13,7 @@ import jakarta.persistence.Table;
 public class Estudante {
     
     @Id 
-    @OneToMany
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long IdEstudante;
     
     @Column(nullable=false)
