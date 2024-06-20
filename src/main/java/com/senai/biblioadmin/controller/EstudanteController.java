ae0e1c573fb0766e7a22dcbf043898eae85876cf
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2024-06-20 11:22:49 -0300

    salvo

--- src/main/java/com/senai/biblioadmin/controller/EstudanteController.java
+++ src/main/java/com/senai/biblioadmin/controller/EstudanteController.java
@@ -4,10 +4,44 @@
  */
 package com.senai.biblioadmin.controller;
 
+import com.senai.biblioadmin.entity.Estudante;
+import com.senai.biblioadmin.entity.Login;
+import com.senai.biblioadmin.service.EstudanteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 /**
  *
  * @author joao_cappeletti
  */
 public class EstudanteController {
+    
+     @Autowired
+    private EstudanteService estudanteService;
+    
+    
+    @PostMapping("/estudante")
+    public ResponseEntity<Long> incluirEstudante(@RequestBody Estudante estudante){
+        
+        Long IdEstudante = estudanteService.incluirEstudante(estudante);
+        if(IdEstudante != null){
+            return new ResponseEntity<>(IdEstudante,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);        
+    }
+    
+    @PostMapping("/estudante/login")
+    public ResponseEntity<Estudante> loginEstudante(@RequestBody Login login){
+        
+        Estudante estudante = estudanteService.loginEstudante(login.getMatricula(),login.getSenha());
+      
+        if(estudante != null){
+            return new ResponseEntity<>(estudante,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);        
+    }
     
 }
