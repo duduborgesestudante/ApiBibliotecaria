25914d14d0a4f3cf3ff9a23ce108a88eb185877c
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-06-20 11:50:10 -0300

    salvo

--- src/main/java/com/senai/biblioadmin/controller/EstudanteController.java
+++ src/main/java/com/senai/biblioadmin/controller/EstudanteController.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.senai.biblioadmin.controller;
 
 import com.senai.biblioadmin.entity.Estudante;
 import com.senai.biblioadmin.entity.Login;
 import com.senai.biblioadmin.service.EstudanteService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 /**
- *
  * @author joao_cappeletti
  */
 public class EstudanteController {
@@ -43,5 +43,38 @@ public class EstudanteController {
         }
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);        
     }
+    @GetMapping("/estudante")
+    public ResponseEntity<List<Estudante>> listarEstudantes(){
+        List<Estudante> listEst = estudanteService.listarEstudantes();
+        if(! listEst.isEmpty()){
+            return new ResponseEntity<>(listEst,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
     
+    @GetMapping("/estudante/matricula/{matricula}")
+    public ResponseEntity<Estudante> consultaEstudantePorMatricula(
+                                   @PathVariable("matricula") Long matricula){
+        Estudante estudante = estudanteService.consultaEstudantePorMatricula(matricula);
+        if(estudante != null){
+            return new ResponseEntity<>(estudante,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    
+    @DeleteMapping("/estudante/{IdEstudante}")
+    public ResponseEntity<Long> excluirCliente(@PathVariable ("IdEstudante")Long IdEstudante){
+        if(estudanteService.excluirEstudante(IdEstudante)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PutMapping("/estudante")
+    public ResponseEntity<Boolean> alterarEstudante(@RequestBody Estudante estudante){
+        
+        if(estudanteService.alterarEstudante(estudante)){
+            return new ResponseEntity<>(true,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 }
