8fc6b5eafe5b81dde6df289efba294616eb7a120
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2024-06-20 11:21:49 -0300

    salvo

---
+++ src/main/java/com/senai/biblioadmin/entity/Login.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senai.biblioadmin.entity;
+
+public class Login {
+    
+    private Long matricula;
+    private String senha;
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+    
+}
--- src/main/java/com/senai/biblioadmin/service/EstudanteService.java
+++ src/main/java/com/senai/biblioadmin/service/EstudanteService.java
@@ -3,6 +3,9 @@ package com.senai.biblioadmin.service;
 
 import com.senai.biblioadmin.entity.Estudante;
 import com.senai.biblioadmin.repository.EstudanteRepository;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -10,24 +13,53 @@ import org.springframework.stereotype.Service;
 public class EstudanteService {
     private EstudanteRepository estudanteRepository;
     
-     public Long incluirEstudante(Estudante estudante){
-        if(estudante.getNome() == null || estudante.getNome() == ""){   
+      public Long incluirEstudante(Estudante estudante){
+        
+        if(estudante.getMatricula() == null || estudante.getNome() == null ||
+           estudante.getSenha() == null || estudante.getEmail() == null){
             return null;
         }
-        if(estudante.getEmail() == null || estudante.getEmail() == ""){
-            return null;
-        }
-        if(estudante.getMatricula() == null){
-            return null;
-        }
-        if(estudante.getSenha() == null || estudante.getSenha() == ""){
-            return null;
-        }
-        if(estudanteRepository.findByMatricula(estudante.getMatricula()) == null){
-            return estudanteRepository.save(estudante).getIdEstudante();
-        }
+        String senhaCod = hashSenha(estudante.getSenha());
+        estudante.setSenha(senhaCod);
+        return estudanteRepository.save(estudante).getIdEstudante();
+    }
+    
+    public Estudante loginEstudante(Long matricula, String senha){
+        
+        String senhaHash = "";
+        Estudante estudanteBD = estudanteRepository.findByMatricula(matricula);
+        if(estudanteBD != null){
+            senhaHash = hashSenha(senha);
+            String senhaBD = estudanteBD.getSenha();
+            //System.out.println("senha do banco...: " + senhaBD);
+            //System.out.println("senha do login...: " + senhaHash);
+            if( senhaHash.equals(senhaBD) ){               
+               return estudanteBD;
+            }
+        }        
         return null;
     }
+    
+    
+    public String hashSenha(String passwd){
+        String passwdCod = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            //md. update(salt);
+            final byte[] hashBytes = md.digest(passwd.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.length; i++) {
+                sb.append(Integer.toString((hashBytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            passwdCod = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.getLocalizedMessage();
+        }
+        return passwdCod;
+    }
+    
+
+
     
     public boolean excluirEstudante(Long IdEstudante){
         if(estudanteRepository.findById(IdEstudante).isPresent()){
