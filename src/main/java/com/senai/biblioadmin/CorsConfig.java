7a95876ee0d629a0b1844b3e5170f110b962314f
duduborges <duduborges333969@gmail.com>
2024-07-11 14:54:32 -0300

    atualizando

--- src/main/java/com/senai/biblioadmin/CorsConfig.java
+++ src/main/java/com/senai/biblioadmin/CorsConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-
 @Configuration
 public class CorsConfig {
 
@@ -17,7 +16,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173"); // Allow requests from this origin
         config.addAllowedOrigin("http://127.0.0.1:5173"); // Allow requests from this origin
-        config.addAllowedOrigin("https://biblioadmin.vercel.app");
+        config.addAllowedOrigin("https://biblioadmin.vercel.app"); // Allow requests from this origin
+        config.addAllowedOrigin("https://189.8.205.53"); 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
